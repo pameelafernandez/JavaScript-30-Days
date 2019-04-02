1ddf9e7e7bb1edbbd2709bbeee253330fbfe3bda
pameelafernandez <pamelamaiden@outlook.es>
2019-04-02 18:05:40 -0300

    cambios en el día 1

--- 01 - JavaScript Drum Kit/drum.js
+++ 01 - JavaScript Drum Kit/drum.js
@@ -1,18 +1,17 @@
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-    
-    if (!audio) return; // stop the function from running all together
-    audio.currentTime = 0; // rewind to the start
-    audio.play();
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
     key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
 }
 
-function removeTransition(e) {
-    if (e.propertyName !== 'transform') return; //skip it if it's not a transform
-    this.classList.remove('playing');
-}
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitioned', removeTransition));
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
